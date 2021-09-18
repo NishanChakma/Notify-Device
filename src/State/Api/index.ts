4b31031f0c73b11578beea47b457e01b1f4059a0
Nishan IT <nishan@m.insta.kim>
2021-09-18 18:57:56 +0600

    .env comment

--- src/State/Api/index.ts
+++ src/State/Api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const callApi = axios.create({
-  baseURL: "http://35.201.2.209:8000/",
+  baseURL: "http://35.201.2.209:8000/", //this should be in .env file while production
   headers: {
     "Content-type": "application/json",
     Accept: "application/json",
